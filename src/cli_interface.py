d60b8b1aeaed6e94be846e263f37597e5434f7d5
abhiraj ranjan <abhiraj.ranjan@outlook.com>
2022-01-27 19:21:06 +0530

    fixed cli printing

--- src/cli_interface.py
+++ src/cli_interface.py
@@ -1,17 +1,26 @@
 from network.main import NetworkManager
 
-print("Alora Python Shell")
-nm = NetworkManager()
-nm.run()
-print("started threads")
-print(f"server running at {nm}")
+intro = "Alora Python Shell\nuse nm to access NetworkManager class"
 
+def start_server():
+    nm = NetworkManager()
+    nm.run()
+    return nm
 
-while a:= input("\n>>> "):
-    a = a.strip()
-    if a == chr(4):
-        break
-    try:
-        print(eval(a))
-    except Exception as e:
-        print(e)
+def run():
+    print(intro)
+    nm = start_server()
+    execute(nm)
+
+def execute(nm: NetworkManager):
+    while a:= input("\n>>> "):
+        a = a.strip()
+        if a == chr(4):
+            break
+        try:
+            print(eval(a))
+        except Exception as e:
+            print(f"[Exception]: {e}")
+
+if __name__ == "__main__":
+    run()

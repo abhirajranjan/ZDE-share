e5867b187b2255e784375e6dcbaaa26a93b353a3
abhiraj ranjan <abhiraj.ranjan@outlook.com>
2022-01-28 06:29:59 +0530

    added "share file" feature

--- src/network/tcpConn.py
+++ src/network/tcpConn.py
@@ -2,9 +2,10 @@ import socket
 import typing
 import threading
 import json
+import pickle
 
 from .abstract import packet_encoding, device_to_connect_at_one_time, \
-                    listener_buffer, EOL, EOF
+                    listener_buffer, EOL, EOF, ClassObject
 from .mcast import LanConnection
 
 # udp multicast (mcast) is to get refreshed list of devices by
@@ -43,7 +44,10 @@ class NetworkManager(LanConnection):
             # new connection obtained
             conn, addr = self.tcp.tcp_listener.accept()
             print(addr, 'connected')
-            client_thread = threading.Thread(target=self.handle_client, args=(conn, addr))
+            classcontainer = ClassObject()
+            # process func handles the process to exec after getting headers 
+            classcontainer.process_function = None
+            client_thread = threading.Thread(target=self.handle_client, args=(conn, addr, classcontainer))
             client_thread.start()
 
     def start_threads(self):
@@ -54,13 +58,17 @@ class NetworkManager(LanConnection):
         self.reset_ip()
         self.start_threads()
 
-    def handle_client(self, conn, addr):
+    def handle_client(self, conn, addr, container):
         data_send_prev = b''
         while True:
             try:
                 data = conn.recv(listener_buffer)
+                print(data)
+                print(container.process_function)
+                if container.process_function:
+                    container.process_function(data, addr, conn, container)
 
-                if (a := data.find(EOL)) != -1:
+                elif (a := data.find(EOL)) != -1:
                     data = data[:a]
                     data_send_prev += data
 
@@ -70,12 +78,30 @@ class NetworkManager(LanConnection):
                         decoded_data = json.loads(data_send_prev.decode(packet_encoding))
                     else:
                         decoded_data = json.loads(data.decode(packet_encoding))
-                    self.process_data(decoded_data, addr, conn)
+                    self.process_data(decoded_data, addr, conn, container)
             except Exception as e:
                 if self.debug:
                     self.except_hook(e)
                 data_send_prev = b''
 
-    def process_data(self, data: dict, addr, conn: socket.socket):
+    def process_data(self, data: dict, addr, conn: socket.socket, container: ClassObject):
         # 1v1 connection
-        print(data, addr)
+        assert "event-type" in data
+        if data["event-type"] == 'data-transfer':
+            assert "data-type" in data
+            assert "metadata" in data
+            assert "filename" in data["metadata"]
+
+            if data["data-type"] == "bytes":
+                print("set process to bytes")
+                container.process_filename = data["metadata"]["filename"]
+                container.process_function = self.process_bytes
+    
+    def process_bytes(self, data: bytes, addr, conn: socket.socket, container: ClassObject):
+        with open(container.process_filename, 'a') as file:
+            file.write(data[:-1].decode(packet_encoding))
+
+        # 'aaff\x01'[-1] gives 1 not b'\x01' thats why id doesnt match with EOF.
+        if data[-1] == EOF[-1]:
+            del container.process_filename
+            container.process_function = None

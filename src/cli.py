229225a80c8d86f0342e52f9d427fb63638f3593
abhiraj ranjan <abhiraj.ranjan@outlook.com>
2022-02-07 02:56:13 +0530

    added cli list command

--- src/cli.py
+++ src/cli.py
@@ -7,6 +7,12 @@ from network.main import NetworkManager
 CLI_ARGUMENTS = {'list', 'connect', 'sendfile', 'ping'}
 
 
+def maxlen(arg: str) -> int:
+    _max = 0
+    for i in arg:
+        _max = max(_max, len(i.name))
+    return _max
+
 def cliparser(ip: typing.Union[str('ipv4'), str('ipv6')] = 'ipv4', debug: bool = False):
     nm_pt = NetworkManager(ip=ip, debug=debug)
     nm_pt.run()
@@ -18,7 +24,15 @@ def cliparser(ip: typing.Union[str('ipv4'), str('ipv6')] = 'ipv4', debug: bool =
 
         if cmd.strip() in CLI_ARGUMENTS:
             if cmd == 'list':
-                print(nm_pt.get_connections())
+                conns = nm_pt.get_connections()
+                _max = maxlen(conns)
+                
+                print('|'+'-'*(_max+8)+'|')
+                for idx, conn in enumerate(conns):
+                    print(('| {:>2} - {:<'+str(_max+1)+'} |').format(idx+1, conn.name))
+                print('|'+'-'*(_max+8)+'|')
+
+                print('\nTo connect type "connect" followed by serial number or name')
                 continue
 
             if cmd == "connect":
